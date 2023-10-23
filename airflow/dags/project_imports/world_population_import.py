35b22688994b0cc773657f7fdd4ca0793ddb9d48
taversa <timna.aversa@spark.net>
2023-10-23 08:32:21 +0200

    extract task done

--- airflow/dags/project_imports/world_population_import.py
+++ airflow/dags/project_imports/world_population_import.py
@@ -1,6 +1,10 @@
+import os
+import boto3
 import requests
+from datetime import datetime, timedelta
+# from tolls import *
 
-def export_excel_file(text:str):
+def export_excel_file():
     """
     Test if dag works on new structure
 
@@ -9,9 +13,26 @@ def export_excel_file(text:str):
     text : string
         string to test result.
     """
-    dls = "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/EXCEL_FILES/1_General/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_REV1.xlsx"
-    resp = requests.get(dls)
+    print('Start of export')
+    url = "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/EXCEL_FILES/1_General/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_REV1.xlsx"
+    print('call function')
+    time = datetime.now()
+    import_online_file_to_s3(url,'world-population-timna-portifolio',
+    f'{time.year}/{time.month}/{time.day}/world_population_{time.year}_{time.month}_{time.day}.xlsx')
+    print("End of export")
 
-    output = open('data/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_REV1.xlsx', 'wb')
-    output.write(resp.content)
-    output.close()
+def import_online_file_to_s3(url, bucket_name, object_key):
+  """Imports an files from the internet into an S3 bucket.
+
+  Args:
+    url: The URL of the file.
+    bucket_name: The name of the S3 bucket.
+    object_key: The key of the object in the S3 bucket.
+  """
+  print(os.getenv('AWS_ACCESS_KEY_ID'))
+  print(os.getenv('AWS_SECRET_ACCESS_KEY'))
+  s3 = boto3.client('s3')
+  print(s3)
+  response = requests.get(url)
+  s3.put_object(Bucket=bucket_name, Key=object_key, Body=response.content)
+
